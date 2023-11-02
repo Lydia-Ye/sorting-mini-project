bb17063c3a8f29e78e02bd81afc0dc4464d27392
yezi <yezi@grinnell.edu>
2023-11-02 13:48:30 -0500

    Update InsertionSort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -8,6 +8,15 @@ import java.util.Comparator;
 
 public class InsertionSort implements Sorter {
 
+  public static void main(String[] args) {
+    Integer[] original = { 1, 4, 4, 5, 1, 5, 6, 3, 6, 3};
+    SORTER.sort(original, (x, y) -> x == y ? 0 : x < y ? -1 : 1);
+
+    for (int i =0; i<original.length; i++) {
+      System.out.println(original[i]);
+    }
+  }
+
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
@@ -33,6 +42,18 @@ public class InsertionSort implements Sorter {
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    for (int i = 1; i < values.length; i++) {
+      T current = values[i];
+
+      // compare current with previous sorted elements to find the correct 
+      // position of current value
+      while ((i - 1) >= 0 && (order.compare(values[i - 1], current) > 0)) {
+        values[i] = values[i - 1];
+        i--;
+      } // while
+
+      // copy current value to the correct postion
+      values[i] = current;
+    } // for
   } // sort(T[], Comparator<? super T>
 } // class InsertionSort
