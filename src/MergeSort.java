3c7bdf14c2e07966e3998e82680346445c63c49f
yezi <yezi@grinnell.edu>
2023-11-02 13:08:19 -0500

    Update MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,10 +3,20 @@ import java.util.Comparator;
 /**
  * Sort using merge sort.
  *
- * @author Your Name Here
+ * @author Lydia Ye
+ * @author Jayson Kunkel (for the codes finished in class for merge sort lab)
  */
 
 public class MergeSort implements Sorter {
+
+  public static void main(String[] args) {
+    Integer[] original = { 1, 4, 4, 5, 1, 5, 6, 3, 6, 3};
+    SORTER.sort(original, (x, y) -> x == y ? 0 : x < y ? -1 : 1);
+
+    for (int i =0; i<original.length; i++) {
+      System.out.println(original[i]);
+    }
+  }
 
   // +--------+------------------------------------------------------
   // | Fields |
@@ -31,8 +41,90 @@ public class MergeSort implements Sorter {
   // | Methods |
   // +---------+
 
+  /**
+   * Sort an array using the merge sort algorithm.
+   */
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    int length = values.length;
+    mergeSort(values,0, length, order);
   } // sort(T[], Comparator<? super T>
+
+  // +-----------------+---------------------------------------------
+  // | Local utilities |
+  // +-----------------+
+
+  /**
+   * Merge the values from positions [lo..mid) and [mid..hi) back into
+   * the same part of the array.
+   *
+   * Preconditions: Each subarray is sorted accorting to comparator.
+   */
+  static <T> void merge(T[] vals, int lo, int mid, int hi, Comparator<? super T> comparator) {
+
+    // Create a temporary empty array to store merged vals
+    T[] merged = java.util.Arrays.copyOfRange(vals, lo, hi);
+
+    int l = lo; // starting index of left array
+    int r = mid; // starting indes of right array
+    int i = 0;
+    int length = hi - lo;
+
+    // copy elements to merged array in order
+
+    while (l < mid && r < hi){
+      if(comparator.compare(vals[l], vals[r]) < 0 || comparator.compare(vals[l], vals[r]) == 0) {
+        // item at left array precedes or two items are equal
+        merged[i] = vals[l];
+        l++;
+        i++;
+      } else{
+        // item at right array precedes
+        merged[i] = vals[r];
+        r++;
+        i++;
+      } // if
+    } // while
+    
+    while (i < length) {
+      if(l >= mid){ 
+        // left array is full, copy all remaining items to right array
+        merged[i] = vals[r];
+        r++;
+        i++;
+      } else if(r >= hi){
+        // right array is full, copy all remaining items to left array
+        merged[i] = vals[l];
+        l++;
+        i++;
+      } // if  
+    } // while
+          
+
+
+    // Replace vals with merged array 
+    int k = 0;
+    for(int j = lo; j < hi; j++){
+      vals[j] = merged[k];
+      k++;
+      
+    } // for 
+  } // merge
+
+  /**
+   * A helper method for sort using merge sort algorithm.
+   */
+  static <T> void mergeSort(T[] vals, int lo, int hi, Comparator<? super T> comparator) {
+    /* Base case */
+    if ((hi - lo) <= 1) {
+      return;
+    } // if
+
+    /* Recursive case */
+    int mid = lo + (hi - lo)/2;
+    mergeSort(vals, lo, mid, comparator);
+    mergeSort(vals, mid, hi, comparator);
+    merge(vals, lo, mid, hi, comparator);
+  } // mergeSort
+
 } // class MergeSort
